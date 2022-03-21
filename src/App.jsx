74cf5beaefa167d185270eb513cdce716fe41e9a
shiwam-C114 <93898678+shiwam-C114@users.noreply.github.com>
2022-03-21 21:21:04 +0530

    state defined and all  stuff working

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("")
+  const [dept, setDept] = useState("")
+  const [gender, setGender] = useState("")
+  const [role, setRole] = useState("")
+  const [salary, setSalary] = useState("")
+
+  function postData() {
+    fetch("http://localhost:3000/employee").then(res=>res.json()).then(console.log)
+    console.log(name,dept, gender,role,salary);
+  }
+
   return (
+  
     <div className="App">
      <h1>Employee</h1> 
      <div>
-      <input type="text" name="" id="" placeholder='name' />
-      <input type="text" name="" id="" placeholder='department' />
-      <input type="text" name="" id="" placeholder='gender' />
-      <input type="text" name="" id="" placeholder='role' />
-      <input type="text" name="" id="" placeholder='salary' />
-      <button>Add employee</button>
+      <input type="text" name="" id="" placeholder='name'  onChange={(e)=>{setName(e.target.value)}} />
+      <input type="text" name="" id="" placeholder='department' onChange={(e)=>{setDept(e.target.value)}} />
+      <input type="text" name="" id="" placeholder='gender' onChange={(e)=>{setGender(e.target.value)}} />
+      <input type="text" name="" id="" placeholder='role' onChange={(e)=>{setRole(e.target.value)}} />
+      <input type="text" name="" id="" placeholder='salary' onChange={(e)=>{setSalary(e.target.value)}} />
+      <button onClick={()=>{postData()}} >Add employee</button>
      </div>
+
 
 
     </div>
