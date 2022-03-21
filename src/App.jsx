e421af3a54957b7aaa0f9565ea37a8d60c150352
shiwam-C114 <93898678+shiwam-C114@users.noreply.github.com>
2022-03-21 20:50:06 +0530

    added template to work

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Employee</h1> 
+     <div>
+      <input type="text" name="" id="" placeholder='name' />
+      <input type="text" name="" id="" placeholder='department' />
+      <input type="text" name="" id="" placeholder='gender' />
+      <input type="text" name="" id="" placeholder='role' />
+      <input type="text" name="" id="" placeholder='salary' />
+      <button>Add employee</button>
+     </div>
+
+
+    </div>
+  );
+}
+
+export default App;
